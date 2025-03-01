a38388503f40fe2193c901fae43764c079e65ae3
ashik <mahmudashik.acce.ru@gmail.com>
2025-03-02 03:53:14 +0600

    Initial Commit

---
+++ Day2/DataTypes.py
@@ -0,0 +1,23 @@
+# Data Types
+
+# Integer
+
+# Just Write like Decimal numbers
+num1 = 10
+num2 = 20
+print(num1 + num2)
+
+# Strings
+String = "Ashik"     # This is a String because this is inside double quotation.
+
+# Subscripting
+SubscriptVar = "Ashik"[0]   # The number 0 inside square bracket indicates the first character of this string.
+SubscriptVar1 = "Ashik"[2]
+print(String)       # This output will be  all the Characters.
+print(SubscriptVar)     # This output will be one character, the first character.
+print(SubscriptVar1)
+
+# for writing large integers we can use underscore like
+print(123_345_45_234_111)   # Computer doesn't read underscores
+
+#
