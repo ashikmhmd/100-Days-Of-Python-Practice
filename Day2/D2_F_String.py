c20799eee07121a3f525728990af68ac67e1d849
ashik <mahmudashik.acce.ru@gmail.com>
2025-03-05 01:05:20 +0600

    Type Casting Challenge

---
+++ Day2/D2_F_String.py
@@ -0,0 +1,14 @@
+# Okay Okay! Nothing Jus Warming Up!
+# Tired of Printing string and integer or float together?
+# Here comes the solution!
+# Use F String!
+# Taking variables in integer!
+
+a = 5
+b = 6
+c = 9
+print("the result of a + b is= " + str((a +b)) + "the result of b - c is = " + str((b- c)))
+# That would give error if i did't add str()
+# This is painful, the solution is F_string
+print(f"the result of a + b is {a+b} and the result if b - c is {b-c}")
+# This one is great! Just use f before double bracket and {} for integer.
