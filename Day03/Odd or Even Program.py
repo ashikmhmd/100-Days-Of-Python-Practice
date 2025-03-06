cccb1b4cd78fe055917170a87ff69517eb4c1604
ashik <mahmudashik.acce.ru@gmail.com>
2025-03-07 00:48:46 +0600

    Lisan Al-Gaib!

---
+++ Day03/Odd or Even Program.py
@@ -0,0 +1,9 @@
+print("Welcome! Here you will able to Identify odd and even")
+Digit = int(input("Type a natural number: "))
+Div = Digit%2
+
+if Div == 0:
+    print(" This is even")
+
+else:
+    print(" This is Odd")
