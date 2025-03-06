a3b274c2bbc131198102aca870ca86ce9850fa7b
ashik <mahmudashik.acce.ru@gmail.com>
2025-03-07 00:28:49 +0600

    Lisan Al-Gaib!

---
+++ Day03/Conditional.py
@@ -0,0 +1,7 @@
+# if else Statement.
+print("Welcome to tarchirar chagoler bacchder bangladesh!")
+eveteaser = str(input("apni ki eveteaser? y/n = "))
+if eveteaser == "y":
+    print("Lisan al-Gaib! apni to amgo touhidi jonotar peer! peer e auliya! Oi, mala pora! nen huzur mala poren!")
+else:
+    print("kpola tui islamer sotru, ja eveteasing kor, maiyader ston ar orna samlanor dayitto tor!")
