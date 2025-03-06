5adc6b1f6d3ed8ee2616cc58a152e2f4c538f4d9
ashik <mahmudashik.acce.ru@gmail.com>
2025-03-07 02:49:54 +0600

    update

--- Day03/Leap Year.py
+++ Day03/Leap Year.py
@@ -1,13 +1,15 @@
 print("Welcome to my leap year Detector!")
 # A leap year is always divided by 4.
-# So I will create a modulas variable.
+# So I will create a modulo variable.
 # First take input.
 year = int(input("Enter a year: "))
-if year%4 == 0:
-    print("This is leap year!")
-elif year%100 == 0:
-    print("this is leap year")
-elif year%400 == 0:
-    print("this is leap year")
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("leap year")
+        else:
+            print("not leap year")
+    else:
+        print("leap year")
 else:
-    print("this is not leap year!")
+    print("This is not leap year!")
